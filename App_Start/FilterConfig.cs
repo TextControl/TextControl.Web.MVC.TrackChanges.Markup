ada13adf963e3a74fefc21a5d3bf6c7c7c7c42ac
Bjoern Meyer <bjoern@textcontrol.com>
2021-02-18 22:18:53 +0100

    initial upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace tx_trackchanges_markup {
+	public class FilterConfig {
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tx_trackchanges_markup.MvcApplication" Language="C#" %>
